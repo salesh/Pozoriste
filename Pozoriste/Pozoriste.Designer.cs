6052cf48933209d8db9f91f42a2422bd89a1cc0e
Sale <Sale@DESKTOP-QJH526B>
2016-11-05 12:36:25 +0100

    Add project files.

---
+++ Pozoriste/Pozoriste.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace Pozoriste
+{
+    partial class Pozoriste
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pozoriste));
+            this.SuspendLayout();
+            // 
+            // Pozoriste
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Pozoriste.Properties.Resources.stage;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Pozoriste";
+            this.Text = "Pozoriste";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
